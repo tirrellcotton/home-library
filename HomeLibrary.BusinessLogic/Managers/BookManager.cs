2e56f9f58ac166d7061e5a8d5194330a78a7560f
Tirrell Cotton <tirrell.cotton@live.com>
2025-09-27 12:45:56 -0500

    Added Book Manager and unit tests

---
+++ HomeLibrary.BusinessLogic/Managers/BookManager.cs
@@ -0,0 +1,15 @@
+﻿using HomeLibrary.Core.Records;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeLibrary.BusinessLogic.Managers;
+
+public class BookManager(HomeLibrarySqlContext context)
+{
+    public async Task<int> GetOwnedBookCountAsync()
+    {
+        return await context.Books
+            .AsNoTracking()
+            .Where(b => b.BookStatusId == BookStatuses.Owned.Id)
+            .CountAsync(); 
+    }
+}
---
+++ HomeLibrary.Core/Records/BookStatuses.cs
@@ -0,0 +1,8 @@
+﻿namespace HomeLibrary.Core.Records;
+
+public record BookStatuses(int Id, string Name)
+{
+    public static BookStatuses Owned { get; } = new BookStatuses(1, "Owned");
+    public static BookStatuses Wishlist { get; } = new BookStatuses(2, "Wishlist");
+    public static BookStatuses Read { get; } = new BookStatuses(3, "Read");
+}
